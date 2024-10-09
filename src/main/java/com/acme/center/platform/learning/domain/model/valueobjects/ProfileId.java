cd2d098c813836e22de6c1d54ec2ce6a6ccb6bfa
Angel Velasquez <pcisavel@upc.edu.pe>
2024-10-09 16:39:57 -0500

    feat(students): add acme student record id, and profile id value objects.

---
+++ src/main/java/com/acme/center/platform/learning/domain/model/valueobjects/ProfileId.java
@@ -0,0 +1,14 @@
+package com.acme.center.platform.learning.domain.model.valueobjects;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public record ProfileId(Long profileId) {
+
+    public ProfileId() { this(0L); }
+
+    public ProfileId {
+        if (profileId < 0)
+            throw new IllegalArgumentException("Profile id cannot be negative");
+    }
+}
