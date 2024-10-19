665324ca4e9442bf05bc7b17a4584f2d75fb5b7f
Angel Velasquez <pcisavel@upc.edu.pe>
2024-10-19 16:29:30 -0500

    feat(courses): add course aggregate root.

---
+++ src/main/java/com/acme/center/platform/learning/domain/model/aggregates/Course.java
@@ -0,0 +1,15 @@
+package com.acme.center.platform.learning.domain.model.aggregates;
+
+import com.acme.center.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Course extends AuditableAbstractAggregateRoot<Course> {
+    private String title;
+    private String description;
+
+    // TODO: Set relationship to LearningPath
+
+}
