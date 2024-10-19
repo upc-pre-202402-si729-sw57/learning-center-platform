b094a25618d66fd5cca3b7d685e207acff32cc5d
Angel Velasquez <pcisavel@upc.edu.pe>
2024-10-19 16:37:02 -0500

    feat(courses): add constructors, default behavior to course aggregate root.

--- src/main/java/com/acme/center/platform/learning/domain/model/aggregates/Course.java
+++ src/main/java/com/acme/center/platform/learning/domain/model/aggregates/Course.java
@@ -1,15 +1,37 @@
 package com.acme.center.platform.learning.domain.model.aggregates;
 
+import com.acme.center.platform.learning.domain.model.valueobjects.LearningPath;
 import com.acme.center.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import lombok.Getter;
+import org.apache.logging.log4j.util.Strings;
 
 @Getter
 @Entity
 public class Course extends AuditableAbstractAggregateRoot<Course> {
     private String title;
     private String description;
+    
+    @Embedded
+    private LearningPath learningPath;
 
-    // TODO: Set relationship to LearningPath
+    public Course(String title, String description) {
+        this();
+        this.title = title;
+        this.description = description;
+    }
+
+    public Course() {
+        this.title = Strings.EMPTY;
+        this.description = Strings.EMPTY;
+        this.learningPath = new LearningPath();
+    }
+
+    public Course updateInformation(String title, String description) {
+        this.title = title;
+        this.description = description;
+        return this;
+    }
 
 }
